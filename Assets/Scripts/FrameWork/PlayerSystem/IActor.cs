978ef2143d0d4e78a3db550753e75298440285e1
lyy19910615 <986837786@qq.com>
2018-07-04 22:33:56 +0800

    actor

---
+++ Assets/Scripts/FrameWork/PlayerSystem/IActor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrameWork
+{
+	public interface IActor
+	{
+		void Move();
+
+		void StopMove();
+
+		void SetPosition(Vector3 position);
+
+		void FaceTo(Vector3 dir);
+
+		
+		void CastSkill();
+
+		bool isAlived{get;set}
+	}
+}
+
